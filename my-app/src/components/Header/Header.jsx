e411363a8247040313eb12a433bb4fb44a005911
Camilo020 <camilo.ortegon020@gmail.com>
2021-10-29 15:40:28 -0500

    pre-estructura de carpetas

---
+++ my-app/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import NavBar from './NavBar/NavBar';
+
+export default class Header extends Component {
+  render () {
+
+    return (
+      <NavBar/>
+    )
+  }
+}
