e7a80b1480738f145788e28cfe722c6ad9d88465
Camilo020 <camilo.ortegon020@gmail.com>
2021-10-30 14:25:40 -0500

    tablas y ajuste

--- my-app/src/pages/Requests/RequestsPage.jsx
+++ my-app/src/pages/Requests/RequestsPage.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from "../../components/Header/Header";
+import TableProjects from "../../components/TableProjects/TableProjects";
 export default class RequestsPages extends Component {
   render () {
     return (
       <div>
         <Header/>
         <h1>RequestsPages</h1>
+        <TableProjects/>
       </div>
     )
   }
